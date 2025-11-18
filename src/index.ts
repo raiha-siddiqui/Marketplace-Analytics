2bec6f75f8607fabcf27dca1112c652afdf3ed20
raiha-siddiqui <raihasiddiqui000@gmail.com>
2025-11-18 08:07:01 +0500

    User deleted

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const prisma= new PrismaClient();
 
 async function main(){
-  const updatedUser= await prisma.user.update({
-    where:{id: '7ef03a9e-1ecf-470a-8d37-49cb65a44ed3'},
-    data: {name: "Raiha Siddiqui"},
+  const deletedUser = await prisma.user.delete({
+    where:{id: '7ef03a9e-1ecf-470a-8d37-49cb65a44ed3'}
   })
-  console.log("User Updated successfully: ", updatedUser )
+  console.log("User deleted successfully: ", deletedUser )
 }
 
 main()
