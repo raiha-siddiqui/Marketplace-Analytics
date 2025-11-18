bf59a0683d87fa9df086fca9d68adc6771c58af1
raiha-siddiqui <raihasiddiqui000@gmail.com>
2025-11-18 08:04:34 +0500

    Updated User

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ import { PrismaClient } from '@prisma/client';
 const prisma= new PrismaClient();
 
 async function main(){
-  const user = await prisma.user.findMany()
-  console.log("All users: ", user)
+  const updatedUser= await prisma.user.update({
+    where:{id: '7ef03a9e-1ecf-470a-8d37-49cb65a44ed3'},
+    data: {name: "Raiha Siddiqui"},
+  })
+  console.log("User Updated successfully: ", updatedUser )
 }
 
 main()
